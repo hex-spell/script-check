8d74892f64ed1d3af891db3fe8ab59ac5fcc3624
Agus <59036817+Vagus-art@users.noreply.github.com>
2021-01-15 02:16:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,18 @@
     };
 
     var myAppJavaScript = function($){
+        var snipplet = `
+        <div class="status panel">
+            <div class="status-icon"><svg height="16" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path d="M4.72 3.22a.75.75 0 011.06 1.06L2.06 8l3.72 3.72a.75.75 0 11-1.06 1.06L.47 8.53a.75.75 0 010-1.06l4.25-4.25zm6.56 0a.75.75 0 10-1.06 1.06L13.94 8l-3.72 3.72a.75.75 0 101.06 1.06l4.25-4.25a.75.75 0 000-1.06l-4.25-4.25z"></path></svg></div>
+            <div
+                class="status-content">
+                <h3 class="heading-small m-none">Hola!</h3>
+                <p class="m-top-quarter m-bottom-none">este es un mensaje de ejemplo en la Thank You Page</p>
+                <div class="custom-payment text-pre-wrap">
+                    <p class="m-bottom-none m-top-quarter"></p>
+                </div>
+        </div>
+        `;
         $( document ).ready(function() {
             $('.status').after('<p>HOLA MUNDO</p>');
         });
